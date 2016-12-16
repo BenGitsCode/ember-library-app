ee274ab337b30107e6a8e5428213bd78da48eb18
benjamski <benjadamski@gmail.com>
2016-12-16 11:47:34 -0500

    Purely example branch
    - Jshint warnings but example works for purposes
    - Demonstrates the distinct differences between the behavior
    of computed properties and observers in Ember.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,5 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisables: true
+  isDisables: true,
+
+  emailAddress: '',
+
+// This computed proprty will only change when yo go and use that proprty
+ actualEmailAddress: Ember.computed('emailAddress', function() { 
+   console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+ }),
+
+// This observer will always be called when the value of the emailAddress changes
+ emailAddressChanged: Ember.observer('emailAddress', function() { 
+   console.log('observer is called', this.get('emailAddress')); 
+ })
+
 });
+
+
+// TODO Teachable moment
+// Open your app in your browser, and activate Ember Inspector. Click on /# Routes section, find the index route, and in the same line, under the Controller column, you will see an >$E sign; click on it. Open the console in Chrome and you will see something like this: Ember Inspector ($E): Class {__nextSuper: undefined, __ember_meta__: Object, __ember1442491471913: "ember443"}
+
+// If you type the following in the console: $E.get('actualEmailAddress'), you should see the console.log output message defined above inside “actualEmailAddress”. You can try out $E.set('emailAddress', 'example@example.com') in the console. What do you see?
+
+// play with this ↑
