c5fb3f6a60eb0598fb0bf6152feb4c0cb49cdb05
benjamski <benjadamski@gmail.com>
2016-12-16 12:25:00 -0500

    Annotate further and correct typo
    - Typo needs to be corrected in actual dev branch
    - Observers vs Computed -- former observes all change
    but only computed will change them where it matters.
    - Note to self, play with this further :nerd_face:wq

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisables: true,
+  isDisabled: true,
 
   emailAddress: '',
 
@@ -21,7 +21,25 @@ export default Ember.Controller.extend({
 
 // TODO Teachable moment
 // Open your app in your browser, and activate Ember Inspector. Click on /# Routes section, find the index route, and in the same line, under the Controller column, you will see an >$E sign; click on it. Open the console in Chrome and you will see something like this: Ember Inspector ($E): Class {__nextSuper: undefined, __ember_meta__: Object, __ember1442491471913: "ember443"}
-
+//
 // If you type the following in the console: $E.get('actualEmailAddress'), you should see the console.log output message defined above inside “actualEmailAddress”. You can try out $E.set('emailAddress', 'example@example.com') in the console. What do you see?
 
 // play with this ↑
+
+
+// Console play with these properties
+//
+// $E.get('actualEmailAddress')
+// undefined
+// $E.get('actualEmailAddress')
+// undefined
+// $E.get('actualEmailAddress')
+// undefined
+// $E.get('emailAddressChanged')
+// undefined
+// $E.set('actualEmailAddress', 'banana@banana.com')
+// "banana@banana.com"
+// $E.get('actualEmailAddress')
+// "banana@banana.com"
+// $E.get('emailAddressChanged')
+// undefined
