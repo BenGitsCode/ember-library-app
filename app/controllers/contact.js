5957ca5fb59ded198a400a48e775b06303177ce9
benjamski <benjadamski@gmail.com>
2016-12-20 14:12:13 -0500

    Started Hw section
    - got interrupted, new branch to keep things organized

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+ contactEmailAddress: '',
+
+
+ isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+ isDisabled: Ember.computed.not('isValid'),
+
+
 });
+
+ // isValid: Ember.computed.gte('contactEmailAddress', 5),
