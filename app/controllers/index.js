950e74beec85459153e7275b2b6e303e41854654
benjamski <benjadamski@gmail.com>
2016-12-23 10:40:49 -0500

    Use store to save emailAddress
    - Successfully tries to save to serve
     - but there is no server yet ¯\_(ツ)_/¯

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
   emailAddress: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
@@ -10,8 +11,11 @@ export default Ember.Controller.extend({
   actions: {
 
     saveInvitation() {
-      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', { email: email });
+
+      this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
       this.set('emailAddress', '');
     }
   }
